47476bce6efb46f2466ff4db62f2734b55923245
이 재욱 <twa072217@naver.com>
2023-06-21 21:21:27 +0900

    그리디_큰 수의 법칙

---
+++ Greedy_2/Greedy_2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	vector<int> v;
+	int n; //배열의 크기
+	int m; //숫자가 더해지는 횟수
+	int k; //연속으로 더할 수 있는 횟수
+	
+	cin >> n >> m >> k;
+
+	for (int i = 0; i < n; i++)
+	{
+		int a;
+		cin >> a;
+		v.push_back(a);
+	}
+	cout << v.size();
+}
