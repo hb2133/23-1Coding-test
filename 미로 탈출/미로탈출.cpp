c1145f5cd1bbfa1ab9a887265422cf22dbae410f
hb2133 <80022610+hb2133@users.noreply.github.com>
2023-07-17 16:40:14 +0900

    미로탈출

--- 미로 탈출/미로탈출.cpp
+++ 미로 탈출/미로탈출.cpp
@@ -1,15 +1,40 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
+const int dx[4] = { -1, 0, 1, 0 };
+const int dy[4] = { 0, 1, 0, -1 };
 int n, m;
 int ary[200][200];
+int dist[200][200];
 
-int BFS(int n, int m)
+bool isValid(int x, int y)
 {
-	if (n < 0 || m < 0)
-		return;
+	return (x >= 0 && y >= 0 && x < n&& y < m);
+}
 
+void BFS(int x, int y)
+{
+	queue<pair<int, int>> q;
+	dist[x][y] = 1;
+	q.push({ x, y });
 
+	while (!q.empty())
+	{
+		pair<int, int> node = q.front(); q.pop();
+
+		for (int i = 0; i < 4; i++)
+		{
+			int newX = node.first + dx[i];
+			int newY = node.second + dy[i];
+
+			if (isValid(newX, newY) && ary[newX][newY] == 1 && dist[newX][newY] == 0)
+			{
+				dist[newX][newY] = dist[node.first][node.second] + 1;
+				q.push({ newX, newY });
+			}
+		}
+	}
 }
 
 int main(void)
@@ -20,21 +45,13 @@ int main(void)
 	{
 		for (int j = 0; j < m; j++)
 		{
-			int k;
-			cin >> k;
-
-			ary[i][j] = k;
+			cin >> ary[i][j];
 		}
 	}
 
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < m; j++)
-		{
-			BFS(n, m);
-		}
+	BFS(0, 0);
 
-	}
+	cout << dist[n - 1][m - 1] << endl;
 
 	return 0;
 }
